6d8c129510bb045c645a4e3fa1aade44625e3bae
mr-leo44 <lionnelkaniki44@gmail.com>
2025-02-04 22:43:03 +0100

    add liste_utilisateur.php

---
+++ liste_utilisateurs.php
@@ -0,0 +1,52 @@
+<?php
+$users = file('users.txt', FILE_IGNORE_NEW_LINES);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Utilissateurs</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <nav>
+    <ul>
+      <li><a href="liste_utilisateurs.php">Listes des utilisateurs</a></li>
+    </ul>
+    </nav>
+  </header>
+  <main>
+    <h1>Tous les utilisateurs</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Nom</th>
+          <th>Email</th>
+          <th>Mot de passe</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+          if (count($users) > 0){
+            foreach ($users as $user){
+              list($nom, $email, $motdepasse) = explode("|", $user);
+              list($nameTitle, $nameContent) = explode(": ", $nom);
+              list($emailTitle, $emailContent) = explode(": ", $email);
+              list($passwordTitle, $passwordContent) = explode(": ", $motdepasse);
+              echo "<tr><td>{$nameContent}</td><td>{$emailContent}</td><td>{$passwordContent}</td></tr>";
+            }
+          } else {
+            echo "<tr><td colspan='3' style='text-align:center;'>Aucun utilisateur enregistré</td></tr>";
+          }
+        ?>
+      </tbody>
+    </table>
+  </main>
+  <footer>
+    <p>&copy;2025 Tous droits réservés.</p>
+  </footer>
+</body>
+</html>
