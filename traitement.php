11ec85c1824a577ff8a64de41bb5634e53d8c86a
mr-leo44 <lionnelkaniki44@gmail.com>
2025-02-04 22:45:43 +0100

    feat check if email is unique on traitement.php

--- traitement.php
+++ traitement.php
@@ -12,6 +12,17 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
     $motdepasseHache= password_hash($motdepasse, PASSWORD_BCRYPT);
   }
 
+  // vérification si l'email existe
+  $users= file('users.txt', FILE_IGNORE_NEW_LINES);
+  // var_dump($users);
+  // die();
+  foreach($users as $user){
+    if (strpos($user, "Email: $email")!== false){
+        echo"Cet email est déjà utilisé";
+        exit;
+    }
+  }
+
   // Ajouter l'utilisateur au fichier
   $userInfo="Nom: $nom |  Email: $email |  Password: $motdepasseHache\n";
   file_put_contents('users.txt', $userInfo, FILE_APPEND);
