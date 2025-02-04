3630ab636201c1ba449131797bc06b7fda8b6bcd
mr-leo44 <lionnelkaniki44@gmail.com>
2025-02-04 22:34:50 +0100

    feat store users in users.txt file

--- traitement.php
+++ traitement.php
@@ -1,13 +1,16 @@
 <?php
 
 if($_SERVER["REQUEST_METHOD"] === "POST"){
-    $nom= $_POST['nom'];
-    $email= $_POST['email'];
-    $motdepasse= $_POST['motdepasse'];
+  $nom= $_POST['nom'];
+  $email= $_POST['email'];
+  $motdepasse= $_POST['motdepasse'];
 
-    if (empty($nom) || empty($email) || empty($motdepasse)) {
-        echo"Tous les champs sont obligatoires.";
-    } 
-    echo"Merci $nom, votre inscription est réussie.";
-    }
-?>
+  if (empty($nom) || empty($email) || empty($motdepasse)) {
+    echo"Tous les champs sont obligatoires.";
+  } 
+
+  // Ajouter l'utilisateur au fichier
+  $userInfo="Nom: $nom |  Email: $email |  Password: $motdepasseHache\n";
+  file_put_contents('users.txt', $userInfo, FILE_APPEND);
+  echo"Merci $nom, votre inscription est réussie.";
+}
